1911ba205683b305293a0f7901f006364ecd0860
Kelden Dradul Dorji <kelden@PC-2.local>
2021-03-28 19:00:27 +0200

    fixed bloody mysql

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,2 +1,5 @@
-export * from './ping.controller';
 export * from './helloworld.controller';
+export * from './ping.controller';
+export * from './product.controller';
+export * from './student.controller';
+
